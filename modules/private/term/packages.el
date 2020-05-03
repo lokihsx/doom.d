37a2cdc856e93c20b2d55b7fefb65d8c84a9cc17
Loki Huang <lokihsx@gmail.com>
2020-05-03 14:16:36 +0800

    add term and markdown support

---
+++ modules/private/term/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; term/term/packages.el
+
+(package! term :built-in t)
+(package! multi-term :pin "7307ddd456db44045206253e5a905d3d8c143d5c")
+(package! eterm-256color)
