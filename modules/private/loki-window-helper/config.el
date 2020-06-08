c9bb2fd60d070b25d9bcec47801cfb03c07d27dc
Loki Huang <lokihsx@gmail.com>
2020-06-08 23:57:21 +0800

    sync

--- modules/private/loki-window-helper/config.el
+++ modules/private/loki-window-helper/config.el
@@ -1,11 +1,19 @@
 ;;; private/loki-window-helper/config.el -*- lexical-binding: t; -*-
 
+(use-package! switch-window
+  :init
+  (global-set-key [remap other-window] #'switch-window)
+  :config
+  (setq switch-window-multiple-frames t
+   switch-window-shortcut-style 'qwerty
+   switch-window-qwerty-shortcuts '("a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'" "z" "x" "c" "v" "b" "n" "m")))
+
 (use-package! framemove
   :load-path "./"
-  :config
-  (setq framemove-hook-into-windmove t)
-  (setq switch-window-multiple-frames t)
+  :init
   (global-set-key (kbd "s-k") 'windmove-up)
   (global-set-key (kbd "s-j") 'windmove-down)
   (global-set-key (kbd "s-h") 'windmove-left)
-  (global-set-key (kbd "s-l") 'windmove-right))
+  (global-set-key (kbd "s-l") 'windmove-right)
+  :config
+  (setq framemove-hook-into-windmove t))
