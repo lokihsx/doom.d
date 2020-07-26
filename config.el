75873972a30a6b5619b02d30441d881058b54202
loki <lokihsx@gmail.com>
2020-07-26 15:32:55 +0800

    make it ao goos

--- config.el
+++ config.el
@@ -53,6 +53,30 @@
 ;; they are implemented.
 ;;
 
-(load! "+emacs-bindings")
 
+;; remember frame status
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
+
+
+(use-package! evil-terminal-cursor-changer
+  :hook (tty-setup . evil-terminal-cursor-changer-activate))
+
+(load! "+emacs-bindings")
 (load! "custom-config")
