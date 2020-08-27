c8d6e37b7a6ee93472120ef8634343dacaa07c49
loki <lokihsx@gmail.com>
2020-08-27 23:56:00 +0800

    remember frame status and restore

--- config.el
+++ config.el
@@ -55,24 +55,24 @@
 
 
 ;; remember frame status
-;; (when-let (dims (doom-store-get 'last-frame-size))
-;;   (cl-destructuring-bind ((left . top) width height fullscreen) dims
-;;     (setq initial-frame-alist
-;;           (append initial-frame-alist
-;;                   `((left . ,left)
-;;                     (top . ,top)
-;;                     (width . ,width)
-;;                     (height . ,height)
-;;                     (fullscreen . ,fullscreen))))))
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
 
-;; (defun save-frame-dimensions ()
-;;   (doom-store-put 'last-frame-size
-;;                   (list (frame-position)
-;;                         (frame-width)
-;;                         (frame-height)
-;;                         (frame-parameter nil 'fullscreen))))
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
 
-;; (add-hook 'kill-emacs-hook #'save-frame-dimensions)
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
 
 (use-package! evil-terminal-cursor-changer
   :hook (tty-setup . evil-terminal-cursor-changer-activate))
@@ -89,9 +89,13 @@
 (after! js2-mode
   (setq js2-basic-offset 2))
 
+(after! vue-mode
+  (add-hook 'vue-mode-hook #'lsp!))
+
 ;(load! "+emacs-bindings")
 (map! :n "gi" #'indent-region)
 (load! "custom-config")
+
 
 ;; work through
 (after! projectile
