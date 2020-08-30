bcddc2bb8b6189f223c228b8942a09ae7104684b
loki <lokihsx@gmail.com>
2020-08-31 02:46:12 +0800

    add pandoc docx template

--- config.el
+++ config.el
@@ -18,7 +18,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two4
-(setq doom-font (font-spec :family "Hack" :size 22))
+;; (setq doom-font (font-spec :family "Hack" :size 22))
+
+(setq doom-font (font-spec :family "Courier New" :size 22))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -27,7 +29,7 @@
 ;;     (setq doom-theme 'doom-spacegrey)
 ;;   (setq doom-theme 'doom-challenger-deep))
 (setq doom-theme 'doom-one)
-;;(setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-dracula)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org/")
