2cda0c605cfc7e82b1068da9522c9c026c9325cf
Loki Huang <lokihsx@gmail.com>
2020-09-04 23:31:38 +0800

    chinese font config

---
+++ proxy.el
@@ -0,0 +1,6 @@
+;;; ~/.doom.d/custom-config.el -*- lexical-binding: t; -*-
+
+(setq url-proxy-services '(("http" . "127.0.0.1:12333")
+                           ("https" . "127.0.0.1:12333")))
+;; (setq url-gateway-method 'socks)
+;; (setq socks-server '("Default server" "127.0.0.1" 1080 5))
