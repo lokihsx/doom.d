738373b29071aa0849025f7dfe165614b1468eb5
Loki Huang <huangshixiong@ainog.com>
2021-10-28 09:40:57 +0800

    make proxy avaliable

--- proxy.el
+++ proxy.el
@@ -1,7 +1,8 @@
 ;;; ~/.doom.d/custom-config.el -*- lexical-binding: t; -*-
 
-(setq url-proxy-services '(("http" . "loki-workstation:8118")
-                           ("https" . "loki-workstation:8118")))
+(setq url-proxy-services '(("http" . "loki-workstation:12333")
+                           ("https" . "loki-workstation:12333")))
 
-(setq url-gateway-method 'socks)
-(setq socks-server '("Default server" "loki-workstation" 1080 5))
+;; NOTE: Don't enable this unless you know what you do!
+;; (setq url-gateway-method 'socks)
+;; (setq socks-server '("Default server" "loki-workstation" 1080 5))
